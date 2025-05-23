a13f82bb7fe8ef63b3412f22dca40e94f5df74b5
Zahra Alvandi <zalvandi39@gmail.com>
2025-05-23 20:54:38 +0330

    part-3

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ const headerMobileLine1 = document.querySelector(".header-mobile-line-1");
 const headerMobileLine2 = document.querySelector(".header-mobile-line-2");
 const headerMobileLine3 = document.querySelector(".header-mobile-line-3");
 const sideMenu = document.querySelector(".side-menu");
+const firstSliderImg = document.querySelector(".first-slider-img");
+const imgBtn1 = document.querySelector(".img-button-1");
+const imgBtn2 = document.querySelector(".img-button-2");
+const imgBtn3 = document.querySelector(".img-button-3");
+const imgBtn4 = document.querySelector(".img-button-4");
+const imgBtn5 = document.querySelector(".img-button-5");
+const sliders1 = document.querySelector(".sliders-1");
+const sliders2 = document.querySelector(".sliders-2");
+const slider2 = document.querySelector(".slider-2")
 
 
 headerMobileMenu.addEventListener("click", function () {
@@ -21,4 +30,18 @@ headerMobileMenu.addEventListener("click", function () {
         headerMobileLine3.style.position = "relative";
         headerMobileLine3.style.top = "0";
     });
+});
+
+imgBtn1.addEventListener("click", function () {
+    firstSliderImg.setAttribute("src", "./image/Computex2025-AGW-Home-Banner-Acer-1_Primary-Hero-S.avif");
+    // sliders1.style.display = "block";
+    sliders2.style.display = "none";
+})
+
+imgBtn2.addEventListener("click", function () {
+    slider2.classList.remove("hide");
+    firstSliderImg.setAttribute("src", "./image/WK22-MemorialDay-2025_Primary-Hero-S.jpg");
+    sliders1.style.display = "none";
+    // sliders2.style.display = "block";
+    sliders2.classList.remove("hide");
 });
